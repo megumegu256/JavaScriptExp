d336173f38b238c64f11feeea7bf28ece4fe69c7
megumegu256 <120536958+megumegu256@users.noreply.github.com>
2024-08-13 01:41:41 +0900

    update dimensio.js and index.html

--- dimensio.js
+++ dimensio.js
@@ -16,16 +16,16 @@ function mult(x, y){
     
     if(x[0].length != y.length) return null;
     
-    var z = new Array(x.length);
-    for (var i = 0; i < x.length; i++) {
+    let z = new Array(x.length);
+    for (let i = 0; i < x.length; i++) {
         z[i] = new Array(y[0].length).fill(0);
     }
 
 
-    for (var i = 0; i < x.length; i++) {
-        for (var j = 0; j < y[0].length; j++) {
+    for (let i = 0; i < x.length; i++) {
+        for (let j = 0; j < y[0].length; j++) {
             z[i][j] = 0;
-            for (var k = 0; k < y.length; k++) {
+            for (let k = 0; k < y.length; k++) {
                 z[i][j] += x[i][k] * y[k][j];
             }
         }
@@ -35,8 +35,8 @@ function mult(x, y){
 
 //n次正方行列定義
 function cMat(n) {
-    var mat = new Array(n);
-    for (var i = 0; i < n; i++) {
+    let mat = new Array(n);
+    for (let i = 0; i < n; i++) {
         mat[i] = new Array(n).fill(0);
     }
     return mat;
@@ -45,7 +45,7 @@ function cMat(n) {
 //単位行列生成
 function E(n){
     mat = cMat(n);
-    for (var i = 0; i < n; i++) {
+    for (let i = 0; i < n; i++) {
         mat[i][i] = 1.0;
     }
     return mat;
@@ -53,11 +53,11 @@ function E(n){
 
 //回転行列生成
 function nrm(n,iv,jv,vt){
-    var z = E(n);
+    let z = E(n);
 
-    var th = Math.PI*vt/180
-    var cv = Math.cos(th);
-    var sv = Math.sin(th);
+    let th = Math.PI*vt/180
+    let cv = Math.cos(th);
+    let sv = Math.sin(th);
 
     z[iv][iv] = cv;
     z[iv][jv] = sv;
@@ -69,12 +69,12 @@ function nrm(n,iv,jv,vt){
 
 //回転後の座標計算
 function rot(n,x,y){
-    var z = x;
-    var t = 0;
-    var jt = 0;
-    var m;
-    for (var i = 0; i < n; i++) {
-        for (var j = 0; j < n-i-1; j++) {
+    let z = x;
+    let t = 0;
+    let jt = 0;
+    let m;
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n-i-1; j++) {
             jt = j+i+1;
             m = nrm(n,i,jt,y[t]);
 
@@ -88,14 +88,14 @@ function rot(n,x,y){
 
 //行列の和の計算
 function sum(x,y,z){
-    for (var i = 0; i < x.length; i++) {
+    for (let i = 0; i < x.length; i++) {
         z[i] = x[i] + y[i];
     }
 }
 
 
-var N = 4;
-var V = [30,0,45,60,0,45];
+let N = 4;
+let V = [30,0,45,60,0,45];
 
 //テキストボックス初期値設定
 let ele_n = document.getElementById('ele_n');
@@ -151,12 +151,12 @@ function buttonClickv(){
 //ボタンクリック時の処理Main
 function buttonClick(){
     //点の座標
-    var f;
+    let f;
     msg_n.innerText = "現在のn=" +N;
     msg_v.innerText = "現在の角度："+V;
 
-    var point = new Array(N);
-    for (var i = 0; i < N; i++) {
+    let point = new Array(N);
+    for (let i = 0; i < N; i++) {
         point[i] = new Array(Math.pow(2,N)).fill(0);
     }
 
@@ -173,11 +173,11 @@ function buttonClick(){
     }
 
     //線の組み合わせ
-    var g;
-    var p;
+    let g;
+    let p;
 
-    var lines = new Array(2);
-    for (var i = 0; i < 2; i++) {
+    let lines = new Array(2);
+    for (let i = 0; i < 2; i++) {
         lines[i] = new Array(comb(N,2)).fill(0);
     }
 
@@ -207,7 +207,7 @@ function buttonClick(){
     */
     /////////////////////////////////////////////////
 
-    var rotpos = rot(N,point,V);
+    let rotpos = rot(N,point,V);
 
 
     //図形の表示
@@ -218,7 +218,7 @@ function buttonClick(){
 
 
     //点の表示
-    var r = 100;
+    let r = 100;
     for (let i = 0; i < rotpos[0].length; i++) {
         ctx.fillStyle = '#000';
         ctx.beginPath();
