af48ffeeec25a36ec5b3f02b02810e7b92d1914d
megumegu256 <120536958+megumegu256@users.noreply.github.com>
2024-08-12 16:48:46 +0900

    update dimensio.js and index.html

--- dimensio.js
+++ dimensio.js
@@ -14,12 +14,12 @@ function comb(n, k){
 
 //行列の積の計算
 function mult(x, y){
-    /*
+    
     if(x[0].length != y.length) return null;
-    */
+    
     var z = new Array(x.length);
     for (var i = 0; i < x.length; i++) {
-        z[i] = new Array(y[0].length).fill(0.0);
+        z[i] = new Array(y[0].length).fill(0);
     }
 
     for (var i = 0; i < x.length; i++) {
@@ -36,7 +36,7 @@ function mult(x, y){
 function cMat(n) {
     var mat = new Array(n);
     for (var i = 0; i < n; i++) {
-        mat[i] = new Array(n).fill(0.0);
+        mat[i] = new Array(n).fill(0);
     }
     return mat;
 }
@@ -68,12 +68,13 @@ function rot(n,x,y){
     var z = x;
     var t = 0;
     var jt = 0;
-    var m = cMat(n);
+    var m;
     for (var i = 0; i < n; i++) {
         for (var j = 0; j < n-i-1; j++) {
             jt = j+i+1;
             m = nrm(n,i,jt,y[t]);
-            z = mult(z,m);
+
+            z = mult(m,z);
         
             t++;
         }
@@ -87,15 +88,62 @@ function sum(x,y,z){
     }
 }
 
-var N = 2;
 
-var point = [[1,0,0],[0,1,0],[0,0,1]];
+var N = 3;
+
+var f;
+
+var point = new Array(N);
+for (var i = 0; i < N; i++) {
+    point[i] = new Array(Math.pow(2,N)).fill(0);
+}
+
+for (var j = 0; j < Math.pow(2,N); j++) {
+    for (var i = 0; i < N; i++) {
+        if (j/(Math.pow(2,i))%2 == 0) {
+            f = -1;
+        }else{ 
+            f = 1;
+        }
+        point[i][j] = f;
+    }
+}
+
+console.log("point:");
+console.log(point);
+
+var g;
+var p;
+
+var lines = new Array(2);
+for (var i = 0; i < 2; i++) {
+    lines[i] = new Array(comb(N,2)).fill(0);
+}
+
+for (let j = 0; j < N; j++) {
+    g = Math.pow(2,N-1);
+    for (let i = 0; i < g; i++) {
+        p = Math.pow(2,j);
+        lines[0][j*g+i] = i+p*(i/p);
+        lines[1][j*g+i] = i+p*(i/p)+p;
+    }
+}
+
+console.log("lines:");
+console.log(lines);
+
 
 var V = [45,45,0,0,0,0,0,0,0];
 
-
-
+console.log("N:");
 console.log(N);
+console.log("point:");
 console.log(point);
+console.log("V:");
 console.log(V);
+
+console.log("mult:");
+console.log(mult(point,point));
+
+console.log("rot:");
 console.log(rot(N,point,V));
