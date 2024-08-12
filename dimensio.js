7d3c1963dae2481f910029f277172bd47f537f1d
megumegu256 <120536958+megumegu256@users.noreply.github.com>
2024-08-12 14:42:44 +0900

    update dimensio.js

--- dimensio.js
+++ dimensio.js
@@ -13,9 +13,15 @@ function comb(n, k){
 }
 
 //行列の積の計算
-function mult(x, y, z){
-    if(x[1].length != y[0].length) return null;
-    
+function mult(x, y){
+    /*
+    if(x[0].length != y.length) return null;
+    */
+    var z = new Array(x.length);
+    for (var i = 0; i < x.length; i++) {
+        z[i] = new Array(y[0].length).fill(0.0);
+    }
+
     for (var i = 0; i < x.length; i++) {
         for (var j = 0; j < y[0].length; j++) {
             z[i][j] = 0;
@@ -24,50 +30,55 @@ function mult(x, y, z){
             }
         }
     }
+    return z;
 }
 
 function cMat(n) {
     var mat = new Array(n);
     for (var i = 0; i < n; i++) {
-        mat[i] = new Array(n).fill(0);
+        mat[i] = new Array(n).fill(0.0);
     }
     return mat;
 }
 
-function E(z,n){
-    z = cMat(n);
+function E(n){
+    mat = cMat(n);
     for (var i = 0; i < n; i++) {
-        z[i][i] = 1;
+        mat[i][i] = 1.0;
     }
+    return mat;
 }
 
-function nrm(n,z,iv,jv,vt){
-    E(z,n);
+function nrm(n,iv,jv,vt){
+    var z = E(n);
 
-    var th = Math.pi*vt/180
+    var th = 3.14159265358979323846*vt/180
     var cv = Math.cos(th);
     var sv = Math.sin(th);
 
     z[iv][iv] = cv;
-    z[iv][jv] = -sv;
-    z[jv][iv] = sv;
+    z[iv][jv] = sv;
+    z[jv][iv] = -sv;
     z[jv][jv] = cv;
+
+    return z;
 }
 
 function rot(n,x,y){
+    var z = x;
     var t = 0;
     var jt = 0;
     var m = cMat(n);
     for (var i = 0; i < n; i++) {
         for (var j = 0; j < n-i-1; j++) {
             jt = j+i+1;
-            nrm(n,m,i,jt,y[t]);
-            mult(x,m,x);
+            m = nrm(n,i,jt,y[t]);
+            z = mult(z,m);
         
             t++;
         }
     }
-
+    return z;
 }
 
 function sum(x,y,z){
@@ -76,10 +87,15 @@ function sum(x,y,z){
     }
 }
 
-var N = 4;
+var N = 2;
 
-var point = [[1,2,3,4],[0,5,3,6],[2,2,2,2],[1,1,1,1]];
+var point = [[1,0,0],[0,1,0],[0,0,1]];
 
-var V = [0,0,0,0,0,0,0,0,0];
+var V = [45,45,0,0,0,0,0,0,0];
 
+
+
+console.log(N);
+console.log(point);
+console.log(V);
 console.log(rot(N,point,V));
